48642ec19106e02e20b9400ca8a9d487bf1dee71
Michal Kóňa <michal.kona@oakrey.cz>
2026-07-01 12:34:15 +0200

    fix: Added the missing URB_NO_TRANSFER_DMA_MAP flag to the method for sending data to USB. Memory for the DMA transfer has already been allocated, so the flag is required.

--- guru-device.c
+++ guru-device.c
@@ -106,8 +106,9 @@ int guru_send_data(struct guru_device *self, uint16_t id, const uint8_t *data,
 	usb_fill_bulk_urb(urb, self->usbdev,
 			  usb_sndbulkpipe(self->usbdev, GURU_DATA_EP), buf,
 			  msg_size, canguru_write_bulk_callback, self);
-
+	urb->transfer_flags |= URB_NO_TRANSFER_DMA_MAP;
 	usb_anchor_urb(urb, &self->tx_submitted);
+
 	err = usb_submit_urb(urb, GFP_ATOMIC);
 	if (err != 0) {
 		dev_err(self->dev, "Unable to submit URB TX (%d).\n", err);
